301d8cb9376cbf9561ff884d23eae257cd4266b6
Meelis Valgeväli <meelis@adm.ee>
2019-02-19 11:20:08 +0200

    7.2.14 -> 7.2.15

--- src/php-7.2/build/modules/build/manifests/php72/extensions/redis.pp
+++ src/php-7.2/build/modules/build/manifests/php72/extensions/redis.pp
@@ -11,18 +11,18 @@ class build::php72::extensions::redis {
     require => File['/tmp/redis-3.0.0.tgz']
   }
 
-  bash_exec { 'cd /tmp/redis-3.0.0 && phpize-7.2.14':
+  bash_exec { 'cd /tmp/redis-3.0.0 && phpize-7.2.15':
     require => Bash_exec['cd /tmp && tar xzf redis-3.0.0.tgz']
   }
 
-  bash_exec { 'cd /tmp/redis-3.0.0 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.2.14 --enable-redis-igbinary':
+  bash_exec { 'cd /tmp/redis-3.0.0 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.2.15 --enable-redis-igbinary':
     timeout => 0,
-    require => Bash_exec['cd /tmp/redis-3.0.0 && phpize-7.2.14']
+    require => Bash_exec['cd /tmp/redis-3.0.0 && phpize-7.2.15']
   }
 
   bash_exec { 'cd /tmp/redis-3.0.0 && make':
     timeout => 0,
-    require => Bash_exec['cd /tmp/redis-3.0.0 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.2.14 --enable-redis-igbinary']
+    require => Bash_exec['cd /tmp/redis-3.0.0 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.2.15 --enable-redis-igbinary']
   }
 
   bash_exec { 'cd /tmp/redis-3.0.0 && make install':
